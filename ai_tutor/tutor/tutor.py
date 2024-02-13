6d52675ca2a75ee2e8ef86cb75afb9a5b9627bca
Nicholas Ifeajika <nifeajika-ctr@wikimedia.org>
2024-02-13 20:26:02 +0000

    update tutor instructions

--- ai_tutor/tutor/tutor.py
+++ ai_tutor/tutor/tutor.py
@@ -5,7 +5,7 @@ from assistant_modes import Mode
 
 class AITutor(pydantic.BaseModel):
     name: str
-    instructions: str = """You are a tutor who is help out a student with their academic course work.
+    instructions: str = """You are a tutor who is helping out a student with their academic course work.
         You are to going to give suggestions on ways to imporve and focus areas to the student based on the data they submit. You will analyse their
         current grades per subject from the submitted files to give the suggestions"""
     model: str="gpt-3.5-turbo-0125"
