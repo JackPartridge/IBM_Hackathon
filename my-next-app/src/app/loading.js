f00e67d96fd23ad8b87e09ececbd863e3da8c782
Jack Partridge <59940643+JackPartridge@users.noreply.github.com>
2024-02-14 06:13:39 +0000

    Delete my-next-app directory

--- my-next-app/src/app/loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-      <div className="flex justify-center items-center h-screen bg-base-100">
-          <span className="loading loading-spinner text-accent w-12 h-12"></span>
-      </div>
-    );
-}
